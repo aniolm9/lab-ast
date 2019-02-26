1ce10c554ca4df4afb67297f87fd058b8acefeee
Aniol Marti <aniol@aniolmarti.cat>
2019-02-26 13:18:02 +0100

    LinkedQueue

--- pract123/src/practica1/LinkedQ/LinkedQueue.java
+++ pract123/src/practica1/LinkedQ/LinkedQueue.java
@@ -4,12 +4,16 @@ import java.util.Iterator;
 import utils.Queue;
 
 public class LinkedQueue<E> implements Queue<E> {
-    private final Node[] queue;
+    private final Node<E>[] queue;
     private final int N;
+    private int nelem; 
+    private int first; 
+    private int last; 
+    
     
     public LinkedQueue(int N) {
         this.N = N;
-        queue = (Node[]) (new Object[N]);
+        queue = (Node[]) (new Node[N]);
     }
     
     @Override
@@ -20,42 +24,62 @@ public class LinkedQueue<E> implements Queue<E> {
 
     @Override
     public int free() {
-        throw new UnsupportedOperationException("Not applies");
+        //throw new UnsupportedOperationException("Not applies");
+        return N-nelem; 
     }
 
     @Override
     public boolean hasFree(int n) {
-        throw new RuntimeException("Aquest mètode s'ha de completar...");
+        //throw new RuntimeException("Aquest mètode s'ha de completar...");
+        return nelem < N;
     }
 
     @Override
     public boolean empty() {
-        throw new RuntimeException("Aquest mètode s'ha de completar...");
+        //throw new RuntimeException("Aquest mètode s'ha de completar...");
+        return nelem == 0; 
     }
 
     @Override
     public boolean full() {
-        throw new RuntimeException("Aquest mètode s'ha de completar...");
+        //throw new RuntimeException("Aquest mètode s'ha de completar...");
+        return nelem == N; 
     }
 
     @Override
     public E peekFirst() {
-        throw new RuntimeException("Aquest mètode s'ha de completar...");
+       //throw new RuntimeException("Aquest mètode s'ha de completar...");
+       if (empty()) throw new IllegalArgumentException("You are trying to acess an empty array");
+       return this.queue[first].getValue();
     }
 
     @Override
     public E peekLast() {
-        throw new RuntimeException("Aquest mètode s'ha de completar...");
+        //throw new RuntimeException("Aquest mètode s'ha de completar...");
+        if (empty()) throw new IllegalArgumentException("You are trying to acess an empty array");
+        return this.queue[last-1].getValue();
     }
 
     @Override
     public E get() {
-        throw new RuntimeException("Aquest mètode s'ha de completar...");
+        //throw new RuntimeException("Aquest mètode s'ha de completar...");
+        if (empty()) throw new IllegalArgumentException("You are trying to acess an empty array");
+        first++;
+        nelem--;
+        this.queue[last-1].setNext(this.queue[first]);
+        return this.queue[first-1].getValue();
     }
 
     @Override
     public void put(E value) {
-        throw new RuntimeException("Aquest mètode s'ha de completar...");
+        //throw new RuntimeException("Aquest mètode s'ha de completar...");
+        if (full()) throw new IllegalArgumentException("You are trying to acess an empty array");
+        this.queue[last] = new Node();
+        this.queue[last].setValue(value);
+        this.queue[last].setNext(this.queue[first]);
+        nelem++;
+        last++;
+        
     }
 
     @Override
@@ -66,15 +90,21 @@ public class LinkedQueue<E> implements Queue<E> {
     class MyIterator implements Iterator {
 
         //Completar
+        private int position = first; 
 
         @Override
         public boolean hasNext() {
-            throw new RuntimeException("Aquest mètode s'ha de completar...");
+            //throw new RuntimeException("Aquest mètode s'ha de completar...");
+            return this.position < size();
         }
 
         @Override
         public E next() {
-            throw new RuntimeException("Aquest mètode s'ha de completar...");
+            //throw new RuntimeException("Aquest mètode s'ha de completar...");
+            if (hasNext()) {
+                return queue[position++].getValue();
+            }
+            return null;
         }
 
         @Override
--- pract123/src/practica1/LinkedQ/TestLQ.java
+++ pract123/src/practica1/LinkedQ/TestLQ.java
@@ -5,7 +5,30 @@ import java.util.Iterator;
 public class TestLQ {
 
     public static void main(String[] args) {
-        throw new RuntimeException("Aquest mètode s'ha de completar...");
-
+        //throw new RuntimeException("Aquest mètode s'ha de completar...");
+        LinkedQueue queue = new LinkedQueue(10);
+        Iterator<Integer> it = queue.iterator();
+        for (int i = 0; i <= 9; i++) {
+            queue.put(i);
+            //System.out.println(i);
+        }
+        while (it.hasNext()) {
+            Integer next = it.next();
+            System.out.print(next);
+            System.out.print(',');
+        }
+        System.out.print('\n');
+        
+        System.out.println("Removing item " + queue.get() + "...");
+        System.out.println("Done.");
+        
+        it = queue.iterator();
+        while (it.hasNext()) {
+            Integer next = it.next();
+            System.out.print(next);
+            System.out.print(',');
+        }
+        System.out.print('\n');
+        
     }
 }

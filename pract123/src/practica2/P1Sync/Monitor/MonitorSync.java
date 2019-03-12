9fa2141e976ba3c9d06e2e5ee8898501c9f8cb06
Aniol Marti <aniol@aniolmarti.cat>
2019-03-12 13:59:14 +0100

    Començar exercici 4

--- pract123/src/practica2/P1Sync/Monitor/MonitorSync.java
+++ pract123/src/practica2/P1Sync/Monitor/MonitorSync.java
@@ -1,22 +1,43 @@
 package practica2.P1Sync.Monitor;
 
 import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MonitorSync {
 
     private final int N;
-    //Completar....
+    private Lock l;
+    private Condition[] queue;
+    private int current;
 
     public MonitorSync(int N) {
         this.N = N;
+        this.l = new ReentrantLock();
+        this.queue = new Condition[N];
+        for (int i = 0; i < N; i++) {
+            queue[i] = l.newCondition();
+        }
     }
 
     public void waitForTurn(int id) {
-        throw new RuntimeException("Aquest mètode s'ha de completar...");
+        l.lock();
+        current = id;
+        l.unlock();
     }
-
+    
     public void transferTurn() {
-        throw new RuntimeException("Aquest mètode s'ha de completar...");
+        l.lock();
+        try {
+            queue[current].await();
+        }
+        catch (InterruptedException ex) {
+            Logger.getLogger(MonitorSync.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        queue[current+1].signal();
+        l.unlock();
+        
     }
 }

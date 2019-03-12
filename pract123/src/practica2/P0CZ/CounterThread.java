3cb9227b0e213a592203c50b8da00fbbee83907e
Aniol Marti <aniol@aniolmarti.cat>
2019-03-12 12:30:33 +0100

    Exercici 1

--- pract123/src/practica2/P0CZ/CounterThread.java
+++ pract123/src/practica2/P0CZ/CounterThread.java
@@ -3,12 +3,13 @@ package practica2.P0CZ;
 public class CounterThread extends Thread {
 
     public static int x;
-    private final int I = 10;
+    private final int I = 5000; // If I>1000 it will probably fail.
 
     @Override
     public void run() {
         for (int i = 0; i < I; i++) {
             x = x + 1;
         }
+        //System.out.println(CounterThread.x);
     }
 }
--- pract123/src/practica2/P0CZ/TestSum.java
+++ pract123/src/practica2/P0CZ/TestSum.java
@@ -1,9 +1,20 @@
 package practica2.P0CZ;
 
+import static java.lang.Thread.sleep;
+
 public class TestSum {
 
     public static void main(String[] args) throws InterruptedException {
-        throw new RuntimeException("Aquest mètode s'ha de completar...");
+        //throw new RuntimeException("Aquest mètode s'ha de completar...");
+        
+        CounterThread th1 = new CounterThread();
+        th1.start();
+        
+        CounterThread th2 = new CounterThread();
+        th2.start();
+        
+        sleep(100);
+        System.out.println(CounterThread.x);
     }
 }
 
